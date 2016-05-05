5061afcd14b168fdeef6dc78ee19b62874b4ddad
caleb.xiang <caleb.xiang@gmail.com>
2016-05-05 10:30:49 +0800

    works

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,2 +1,10 @@
 class Lot < ActiveRecord::Base
+  def display_num
+    if self.half == "后半场"
+      extra = Lot.where(group: self.group, half: "前半场").count
+      return self.number + extra
+    else
+      return self.number
+    end
+  end
 end
