927f26a225049a4602349bf50c0d37f55e95a37a
caleb.xiang <caleb.xiang@gmail.com>
2016-05-25 09:58:41 +0800

    rerun

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,18 @@ ActiveRecord::Schema.define(version: 20160503021507) do
   create_table "kids", force: :cascade do |t|
     t.string   "name"
     t.string   "school"
-    t.string   "who"
     t.string   "phone"
     t.string   "district"
     t.string   "song_a"
     t.string   "song_b"
     t.string   "category"
     t.string   "group"
-    t.string   "half"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "lots", force: :cascade do |t|
     t.string   "group"
-    t.string   "half"
     t.integer  "number"
     t.integer  "kid_id"
     t.datetime "created_at", null: false
